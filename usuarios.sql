cde4842f68f6509aa8d9402a50944fc8dd7f5fff
Higor Bechelli <hbechelli@gmail.com>
2016-07-01 04:52:42 -0300

    Cadastro de Cliente

--- usuarios.sql
+++ usuarios.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 01-Jul-2016 às 07:01
+-- Generation Time: 01-Jul-2016 às 09:47
 -- Versão do servidor: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -34,19 +34,21 @@ CREATE TABLE `usuarios` (
   `numero` varchar(10) NOT NULL,
   `bairro` varchar(100) NOT NULL,
   `cep` varchar(9) NOT NULL,
-  `sim` varchar(100) NOT NULL,
-  `nao` varchar(1000) NOT NULL
+  `positivo` int(100) NOT NULL,
+  `negativo` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`nome`, `email`, `telefone`, `endereco`, `numero`, `bairro`, `cep`, `sim`, `nao`) VALUES
-('Higor Bechelli de Oliveira', 'hbechelli@gmail.com', '4799151476', 'Rua Rolf F. Penski', '250', 'Vila Nova', '89237-813', '', ''),
-('Maria das Couves', 'maria.couve@hotmail.com', '45 78901234', 'Av. Querubim', '334', 'Centro', '89098-000', '', ''),
-('Eduardo Mattos', 'mattos@gmail.com', '46 99879877', 'Rua Toda a vida irÃ¡s reto', '666', 'Vila Pequena', '88449-090', 'Sim', 'NÃ£o'),
-('Paulo Dallagno', 'paulo_aur2@hotmail.com', '47 99151476', 'Rua Rolf F. Pensk', '250', 'Vila Nova', '89237-813', '', '');
+INSERT INTO `usuarios` (`nome`, `email`, `telefone`, `endereco`, `numero`, `bairro`, `cep`, `positivo`, `negativo`) VALUES
+('Ana', 'a.merroto@hotmail.com', '41 8789-900', 'ServidÃ£o das Flores', 's/n', 'Floresta', '89676-456', 0, 0),
+('Marcelo Cabral', 'cabral@brturbo.com.br', '55 33439009', 'Rua Dos TrÃªs', '3', 'Costa Sul', '87789-098', 0, 0),
+('Higor Bechelli de Oliveira', 'hbechelli@gmail.com', '4799151476', 'Rua Rolf F. Penski', '250', 'Vila Nova', '89237-813', 0, 0),
+('Maria das Couves', 'maria.couve@hotmail.com', '45 78901234', 'Av. Querubim', '334', 'Centro', '89098-000', 0, 0),
+('Eduardo Mattos', 'mattos@gmail.com', '46 99879877', 'Rua Toda a vida irÃ¡s reto', '666', 'Vila Pequena', '88449-090', 0, 0),
+('Paulo Dallagno', 'paulo_aur2@hotmail.com', '47 99151476', 'Rua Rolf F. Pensk', '250', 'Vila Nova', '89237-813', 0, 0);
 
 --
 -- Indexes for dumped tables
