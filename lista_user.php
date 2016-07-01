090d9046f92d8f44e4db6822d362139f482bb631
Higor Bechelli <hbechelli@gmail.com>
2016-07-01 04:15:17 -0300

    Cadastro de Cliente

--- lista_user.php
+++ lista_user.php
@@ -3,8 +3,9 @@
 $result = mysql_query ("SELECT nome, email, telefone, endereco FROM usuarios", $conn);
 while (list ($nome, $email, $telefone, $endereco) = mysql_fetch_row ($result)) {
      echo " <tr>\n" .
-           "  <td><a href=\"info.php?id=$id\">$nome</a></td>\n" .
-           "  <td>$salario</td>\n" .
+           "  <td><a href=\"cadastro.php?email=$email\">$nome</a></td>\n" .
+           "  <td>$telefone</td>\n" .
+           "  <td>$endereco</td>\n" .
             " </tr>\n";
 }
 
